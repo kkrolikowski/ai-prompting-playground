110387d576b097a1c4a80577b6026b4b0ef9d571
Krzysztof Królikowski <kkrolikowski@gmail.com>
2025-01-04 21:40:05 +0100

    style: update indentation

--- lib/aimodel.py
+++ lib/aimodel.py
@@ -2,25 +2,25 @@ import openai
 from jinja2 import Environment, FileSystemLoader
 
 class AIModel:
-  def __init__(self, api_key):
-    self.model = "gpt-4o"
-    self.tpl_dir = "templates"
-    self.api_key = api_key
+    def __init__(self, api_key):
+        self.model = "gpt-4o"
+        self.tpl_dir = "templates"
+        self.api_key = api_key
 
-  def complete(self, prompt):
-    try:
-      res = openai.chat.completions.create(
-        model=self.model, messages=[{"role": "user", "content": prompt}]
-      )
-      return res.choices[0].message.content
-    except Exception as err:
-      print(f"Error on querying OpenAI: {err}")
+    def complete(self, prompt):
+        try:
+            res = openai.chat.completions.create(
+                model=self.model, messages=[{"role": "user", "content": prompt}]
+            )
+            return res.choices[0].message.content
+        except Exception as err:
+            print(f"Error on querying OpenAI: {err}")
 
-  def prompt_render(self, prompt_meta):
-    try:
-      env = Environment(loader=FileSystemLoader(self.tpl_dir))
-      template = env.get_template(f"{prompt_meta['template']}.j2")
+    def prompt_render(self, prompt_meta):
+        try:
+            env = Environment(loader=FileSystemLoader(self.tpl_dir))
+            template = env.get_template(f"{prompt_meta['template']}.j2")
 
-      return template.render(**prompt_meta["variables"])
-    except Exception as err:
-      print(f"Template error occured: {err}")
+            return template.render(**prompt_meta["variables"])
+        except Exception as err:
+            print(f"Template error occured: {err}")
--- run.py
+++ run.py
@@ -8,8 +8,8 @@ load_dotenv()
 ai = AIModel(os.getenv("OPENAI_API_KEY"))
 
 prompt = ai.prompt_render({
-  "variables": {"article": "What is data engineering?", "format": "yaml"},
-  "template": "article_outline"
+    "variables": {"article": "What is data engineering?", "format": "yaml"},
+    "template": "article_outline"
 })
 
 print(ai.complete(prompt))
